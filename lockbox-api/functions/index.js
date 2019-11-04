8899c8aee30c7760c070503d9b86f822213fd1f9
Benzt2h <nattapong.wo58@snru.ac.th>
2019-11-04 16:37:39 +0700

    get all and one.

--- lockbox-api/functions/index.js
+++ lockbox-api/functions/index.js
@@ -1,8 +1,28 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin')
+const express = require("express")
+const cors = require('cors')
+admin.initializeApp(functions.config().firebase)
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+let db = admin.database()
+
+const app = express()
+
+app.use(cors({ origin: true }))
+
+app.get('/', (req, res, next) => {
+    let data = db.ref("locker")
+    data.once("value", (snapshot) => {
+        res.send(snapshot.val())
+    })
+})
+
+
+app.get('/test', (req, res, next) => {
+    let data = db.ref("locker").orderByChild("id").equalTo(1);
+    data.on("value", (snapshot) => {
+        res.send(snapshot.val())
+    })
+})
+
+exports.lockBox = functions.https.onRequest(app);
