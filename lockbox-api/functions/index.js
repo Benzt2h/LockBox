d202b60c34a82b2ba0e8c58a0e7156551da7cca9
Benzt2h <nattapong.wo58@snru.ac.th>
2019-11-04 19:14:28 +0700

    fix bug get one and add update

--- lockbox-api/functions/index.js
+++ lockbox-api/functions/index.js
@@ -18,11 +18,18 @@ app.get('/', (req, res, next) => {
 })
 
 
-app.get('/test', (req, res, next) => {
-    let data = db.ref("locker").orderByChild("id").equalTo(1);
-    data.on("value", (snapshot) => {
+app.get('/:id', (req, res, next) => {
+    let id = parseInt(req.params.id)
+    let data = db.ref("locker").orderByChild("id").equalTo(id)
+    data.once("child_added", (snapshot) => {
         res.send(snapshot.val())
     })
+})
+
+app.put('/:id', (req, res, next) => {
+    let id = parseInt(req.params.id) - 1
+    db.ref("locker/" + id).update(req.body)
+    res.send({ msg: "บันทึกข้อมูลสำเร็จ" })
 })
 
 exports.lockBox = functions.https.onRequest(app);
